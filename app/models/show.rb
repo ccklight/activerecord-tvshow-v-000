be206fb796a89ebbaf52ab4185cd8d93ca0246a3
Cheryl K <cherylkelson@icloud.com>
2019-01-24 23:52:11 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -26,12 +26,9 @@ class Show < ActiveRecord::Base
 
 
   def self.popular_shows
-    self.where("rating > 5", self.highest_rating).first
+    self.where("rating > ?", 5) self.highest_rating).first
 #Use the where Active Record method
   end
-
-       self.where("rating >= ?", 5)
-
    
   # def shows_by_alphabetical_order
     #Use the order Active Record method
