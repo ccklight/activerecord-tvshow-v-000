9e36d53d5c1787ff6650d0a223808640c3007d4b
Cheryl K <cherylkelson@icloud.com>
2019-01-24 23:12:09 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -9,22 +9,22 @@ class Show < ActiveRecord::Base
 #use highest_rating as helper method here
   # end
 
-  # 
+  #
   # def lowest_rating
-  # 
+  #
   # end
 
 
 
   # def least_popular_show
-  # 
+  #
   # end
-  # 
-  # 
+  #
+  #
   # def ratings_sum
-  # 
+  #
   # end
-  # 
+  #
 
   # def popular_shows
 #Use the where Active Record method
