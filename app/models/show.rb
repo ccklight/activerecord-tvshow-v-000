b7dc51ff5c04b995c9084f28f090a1e2bac5352c
Cheryl K <cherylkelson@icloud.com>
2019-01-24 23:01:51 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,12 +1,10 @@
 class Show < ActiveRecord::Base
 
-  def highest_rating 
+  def highest_rating
 
-  end 
+  end
 
 
   def most_popular_show
 #use highest_rating as helper method here 
   end
-
-  
