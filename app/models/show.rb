5e14e0f33b1a782bd71a560527062812c3807d8d
Cheryl K <cherylkelson@icloud.com>
2019-01-24 23:35:06 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -5,11 +5,11 @@ class Show < ActiveRecord::Base
   end
 
   def self.most_popular_show
-
+    self.where("rating = ?", self.highest_rating)
   end
-
+# Song.where("number_of_stars > ?", 3)
   **********
-  def self.most_popular_show
+
        self.where("rating = ?", self.highest_rating).first
      end
 **********
